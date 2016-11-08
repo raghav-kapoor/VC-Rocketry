19a4b1dfb7532cd0ba9f726da9925a31bcc852fc
Daryan Qawami <Daryan Qawami>
2016-11-07 16:28:59 -0800

    Includes time measurement for reading 1000 readings

--- flight/getFrame.py
+++ flight/getFrame.py
@@ -18,15 +18,14 @@ ina219B = INA219(0x41)
 
 #Stores many frames of data
 frame = []
+num = 0  #num is a test value
 
 """Generates frames of data that contain: raw acceleration values,
-magnetometer, gps, add on later
+magnetometer, gps, and etc. and adds it to the frames array
 """
-while True:
-    print getFrame()
 
 def getFrame():
-    time_stamp = {"a_x": round(motion.accelerometer().x), roundOff),    #x-axis acceleration
+    raw_data = {"a_x": round(motion.accelerometer().x), roundOff),    #x-axis acceleration
     "a_y": round(motion.accelerometer().y, roundOff),                   #y-axis acceleration
     "a_z": round(motion.accelerometer().z, roundOff),                   #z-axis acceleration    
     "t": round(weather.temperature(), roundOff),                        #Temperature
@@ -42,8 +41,14 @@ def getFrame():
     "alt": round(gpsd.fix.altitude, roundOff),                          #Altitude
     "sp": round(gpsd.fix.speed, roundOff)}                              #Speed
 
-    linux_time = str((round(time.time(), 4)))
-    exec("%s" = time_stamp % (linux_time))
-    
-    
-    
+    #Converts time stamp to variable name and assigns it the dictionary values
+    linux_time = str((round(time.time(), roundOff)))
+
+    frame.append((var()[linux_time] = raw_data))
+
+start_time = time.time()    
+while (num < 1000):
+    getFrame()
+    num++
+print("Time to run: " + (time.time() - start_time))
+
