3e6c7c19c79953df4ca1538871096f33a0c9020a
Pi28 <Pi28@vcs.net>
2016-11-12 00:02:55 +0000

    Round off acceleration and velocity to one decimal

--- flight/velocityTest.py
+++ flight/velocityTest.py
@@ -11,20 +11,20 @@ import RPi.GPIO as GPIO
 import serial
 
 frame = []
-roundOff = 4
+roundOff = 1
 
 def getFrame():
     raw_data = { "linux_time": (time.time()),
                  "a_x": round(motion.accelerometer().x, roundOff),
-                 "a_y": round(motion.accelerometer().y, roundOff),
+                 "a_y": round(motion.accelerometer().y, roundOff)
                  }
     frame.append(raw_data)
 
 def velocityX():
-    return (((frame[-1]["a_x"])+(frame[-2]["a_x"])) * .5) * (((frame[-1]["linux_time"])) - ((frame[-2]["linux_time"])) + v_xi)
+    return (((frame[-1]["a_x"])+(frame[-2]["a_x"]) * .5) * ((frame[-1]["linux_time"]) - (frame[-2]["linux_time"])) + v_xi)    
 
 def velocityY():
-    return (((frame[-1]["a_y"])+(frame[-2]["a_y"])) * .5) * (((frame[-1]["linux_time"])) - ((frame[-2]["linux_time"])) + v_yi)
+    return (((frame[-1]["a_y"])+(frame[-2]["a_y"]) * .5) * ((frame[-1]["linux_time"]) - (frame[-2]["linux_time"])) + v_yi)
 
 i = 0
 v_xi = 0
@@ -36,8 +36,8 @@ while True:
     print (frame[-1]["a_y"])
     #if (abs(frame[-1]["a_x"])  > 0.1 and abs(frame[-1]["a_y"]) > 0.1):
     getFrame()
-    print(velocityX())
-    print(velocityY())
+    print(round(velocityX(), roundOff))
+    print(round(velocityY(), roundOff))
     v_xi = velocityX() 
     v_yi = velocityY()
     print
@@ -45,8 +45,8 @@ while True:
     i = i + 1
     if i == 2:
         getFrame()
-        p_x = ((velocityX() + v_xi) * 0.5) * ((frame[-1]["linux_time"])) - ((frame[-4]["linux_time"]))
-        p_y = ((velocityY() + v_yi) * 0.5) * ((frame[-1]["linux_time"])) - ((frame[-4]["linux_time"]))
+        p_x = (round(((velocityX() + v_xi) * 0.5) * ((frame[-1]["linux_time"]) - (frame[-4]["linux_time"]))), roundOff)
+        p_y = (round(((velocityY() + v_yi) * 0.5) * ((frame[-1]["linux_time"]) - (frame[-4]["linux_time"]))), roundOff)
         print(p_x)
         print(p_y)
         print()
