391708f331a81236186868c18696e25d119aeeb2
Pi28 <Pi28@vcs.net>
2017-01-12 23:47:38 +0000

    Updating unpushed files. Closed serial port for xbee in send and receive

--- xbee/xbee_receive.py
+++ xbee/xbee_receive.py
@@ -2,6 +2,7 @@ import serial
 from time import sleep
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
 out = ''
-while True:
-	out = ser.read()
+for i in range(100):
+	out = ser.read(1)
 	print 'Received %s' % out
+ser.close()
