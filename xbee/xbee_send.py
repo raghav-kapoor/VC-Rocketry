ae456899bdebb3db497330d388f81aa7ed45ba2e
Pi253 <pi253@vcs.net>
2017-01-18 00:23:21 +0000

    Updated transmitting and receiving code

--- xbee/xbee_send.py
+++ xbee/xbee_send.py
@@ -1,13 +1,8 @@
 import serial
 from time import sleep
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-string = ""
-<<<<<<< HEAD
-for i in range(10000000):
-  print i 
-=======
+string = "" 
 for i in range(100): 
   print i
->>>>>>> 391708f331a81236186868c18696e25d119aeeb2
   ser.write(str(i))
 ser.close()
