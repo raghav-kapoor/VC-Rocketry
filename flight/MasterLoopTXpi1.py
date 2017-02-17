023e68de7bc428e7f5fe09d2ae02e29bf1448268
ABLV2539 <alexander.vu@warriorlife.net>
2017-02-17 15:36:15 -0800

    Updating launch checks

--- flight/MasterLoopTXpi1.py
+++ flight/MasterLoopTXpi1.py
@@ -240,13 +240,12 @@ def sendFrame():
 	print(str(currentFrame))
 
 def apogeeCheck():
-	#Check for change in pressure and store in pressureChange
-	if (mode == "flight") and abs(corrAltArray[-1] - corrAltArray[-10] < 5):
+	if (mode == 3) and abs(corrAltArray[-1] - corrAltArray[-10] < 5):
 		return True
 	return False
 
 def landCheck():
-	if (mode == "descent" and (abs(corrAltArray[-1] - corrAltArray[-40]) < 1)):
+	if (mode == 4) and (abs(corrAltArray[-1] - corrAltArray[-40]) < 1):
 		return True
 	return False
 
@@ -271,19 +270,20 @@ while True:
 			sendFrame()
 			if frame[-1]["volt_b2"] > 9.0:
 				flightMode = 2
-		launchFlag = False
 		while flightMode == 2:
 			sendFrame()
 			if abs(frame[-1]["a_z"]) > 1.5:
-				launchFlag = True
-			if launchFlag and apogeeCheck():
+				flightMode = 3
+		while flithMode == 3:
+			sendFrame()
+			if apogeeCheck():
 				#trigger squib
 				squibDeployed = 1
-				flightMode = 3
-		while flightMode == 3:
+				flightMode = 4
+		while flightMode == 4:
 			sendFrame()
 			if landCheck():
-				flightmode = 4
+				flightmode = 5
 	except(KeyboardInterrupt):
 		gpsp.running = False
 		gpsp.join()
