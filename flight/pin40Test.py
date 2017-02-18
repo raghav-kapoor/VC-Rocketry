b2e0cda076f8f509854d02750f37c835604f5f16
ABLV2539 <alexander.vu@warriorlife.net>
2017-02-17 16:19:54 -0800

    Update pin40Test.py

--- flight/pin40Test.py
+++ flight/pin40Test.py
@@ -8,17 +8,19 @@ roundOff = 3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.OUT)
-try:
-	while True:
-		GPIO.output(20, 1)
-		print "on"
-		print("Voltage:  " + str(round(ina219A.getBusVoltage_V(), roundOff)))
-		print("Current:  " + str(round(ina219A.getCurrent_mA(), roundOff)))
-		sleep(1)
+while True:
+	try:
 		GPIO.output(20, 0)
 		print "off"
 		print("Voltage:  " + str(round(ina219A.getBusVoltage_V(), roundOff)))
 		print("Current:  " + str(round(ina219A.getCurrent_mA(), roundOff)))
 		sleep(1)
-except KeyboardInterrupt:
-	GPIO.cleanup()
+	except (KeyboardInterrupt)
+		GPIO.output(20, 1)
+		print "on"
+		for i in range(10):
+			print("Voltage:  " + str(round(ina219A.getBusVoltage_V(), roundOff)))
+			print("Current:  " + str(round(ina219A.getCurrent_mA(), roundOff)))
+			sleep(1)
+		GPIO.cleanup()
+		break
