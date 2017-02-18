1692016f785ca7f76715e4e59464ff1e6c54b457
ABLV2539 <alexander.vu@warriorlife.net>
2017-02-17 16:09:18 -0800

    Added current and power sensors to test file

--- flight/pin40Test.py
+++ flight/pin40Test.py
@@ -1,5 +1,10 @@
+import sys
 from time import sleep
+from Subfact_ina219 import INA219
 import RPi.GPIO as GPIO
+
+ina219A = INA219(0x40)
+roundOff = 3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.OUT)
@@ -7,9 +12,13 @@ try:
 	while True:
 		GPIO.output(20, 1)
 		print "on"
+		print("Voltage:  " + str(round(ina219A.getBusVoltage_V(), roundOff)))
+		print("Current:  " + str(round(ina219A.getCurrent_mA(), roundOff)))
 		sleep(1)
 		GPIO.output(20, 0)
 		print "off"
+		print("Voltage:  " + str(round(ina219A.getBusVoltage_V(), roundOff)))
+		print("Current:  " + str(round(ina219A.getCurrent_mA(), roundOff)))
 		sleep(1)
 except KeyboardInterrupt:
 	GPIO.cleanup()
