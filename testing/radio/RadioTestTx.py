ac228366d9cab66aae93592a688dc196bc39d451
ABLV2539 <alexander.vu@warriorlife.net>
2017-06-03 15:54:19 -0700

    Update RadioTestTx.py

--- testing/radio/RadioTestTx.py
+++ testing/radio/RadioTestTx.py
@@ -3,10 +3,12 @@ from time import sleep
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 message = "AA"
 message = message + "M" * 39
-message = message + "ZZ"
+count=0
 while True:
   try:
-    ser.write(message)
+    count++
+    tempMessage = message + str(count).zfill(4)
+    ser.write(tempMessage)
     time.sleep(0.04)
   except KeyboardInterrupt:
     break
