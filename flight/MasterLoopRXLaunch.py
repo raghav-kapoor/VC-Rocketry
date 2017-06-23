208fd3f4b45002cd3e8e296220affbeff274562e
raghav-kapoor <ragu.kapoor@yahoo.com>
2017-06-23 20:13:39 +0000

    Added MasterLoopRXLaunch.py with data displaying interface

---
+++ flight/MasterLoopRXLaunch.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+import time
+import serial
+import sys
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+rxfile = open("testreceive.txt", "w")
+FRAMESIZE= 39
+DECSIZE = 93
+
+
+def asciiToBin(x):
+	binStr = ''
+	for i in x:
+		binStr+=str(((bin(ord(i)))[2:]).zfill(8))
+	return binStr
+
+def binToDec(x):
+	return(str(int(str(x),2)))
+
+def write(line):
+	sys.stdout.write(line)
+	sys.stdout.flush()
+
+class DataPoint:
+	#name in dictionary
+	name = ""
+	#head digit for positivity
+	hasPositivity = 0
+	#amount of digits including the head
+	length = 0
+	#digits decimal offset count
+	decOffset = 0
+
+	def __init__(self, name, hasPositivity, length, decOffset):
+		self.name = name
+		self.hasPositivity = hasPositivity
+		self.length = length
+		self.decOffset = decOffset
+
+def frameAssembly(testFrame):
+	cAssembly = ""
+	cAssembly += str(int(testFrame["time"]*1000))
+	cAssembly += str(testFrame["flight_mode"])
+	cAssembly += str(testFrame["squib_deployed"])
+	cAssembly += str(abs(int(testFrame["temp"]*10))).zfill(4)[-4:]
+	cAssembly += str(abs(int(testFrame["pressure"]/10))).zfill(5)[-5:]
+	cAssembly += str(abs(int(testFrame["current_1"]*10))).zfill(4)[-4:]
+	cAssembly += str(abs(int(testFrame["volt_b1"]*1000))).zfill(4)[-4:]
+	cAssembly += str(abs(int(testFrame["current_2"]*10))).zfill(4)[-4:]
+	cAssembly += str(abs(int(testFrame["volt_b2"]*1000))).zfill(4)[-4:]
+	cAssembly += "0" + str(int(testFrame["gps_lat"]*1000000)).zfill(9)[-9:] if testFrame["gps_lat"] > 0 else "1" + str(abs(int(testFrame["gps_lat"]*1000000))).zfill(9)[-9:]
+	cAssembly += "0" + str(int(testFrame["gps_lon"]*1000000)).zfill(9)[-9:] if testFrame["gps_lon"] > 0 else "1" + str(abs(int(testFrame["gps_lon"]*1000000))).zfill(9)[-9:]
+	cAssembly += str(abs(int(testFrame["gps_alt"]*10))).zfill(5)[-5:]
+	cAssembly += str(abs(int(testFrame["gps_spd"]*10))).zfill(4)[-4:]
+	cAssembly += "0" + str(int(testFrame["a_x"]*10)).zfill(3)[-3:] if testFrame["a_x"] > 0 else "1" + str(abs(int(testFrame["a_x"]*10))).zfill(3)[-3:]
+	cAssembly += "0" + str(int(testFrame["a_y"]*10)).zfill(3)[-3:] if testFrame["a_y"] > 0 else "1" + str(abs(int(testFrame["a_y"]*10))).zfill(3)[-3:]
+	cAssembly += "0" + str(int(testFrame["a_z"]*10)).zfill(3)[-3:] if testFrame["a_z"] > 0 else "1" + str(abs(int(testFrame["a_z"]*10))).zfill(3)[-3:]
+	cAssembly += "0" + str(int(testFrame["mag_x"]/10)).zfill(3)[-3:] if testFrame["mag_x"] > 0 else "1" + str(abs(int(testFrame["mag_x"]/10))).zfill(3)[-3:]
+	cAssembly += "0" + str(int(testFrame["mag_y"]/10)).zfill(3)[-3:] if testFrame["mag_y"] > 0 else "1" + str(abs(int(testFrame["mag_y"]/10))).zfill(3)[-3:]
+	cAssembly += "0" + str(int(testFrame["mag_z"]/10)).zfill(3)[-3:] if testFrame["mag_z"] > 0 else "1" + str(abs(int(testFrame["mag_z"]/10))).zfill(3)[-3:]
+	return cAssembly
+
+def frameDisassembly(decAssembly):
+	if (len(decAssembly) != DECSIZE):
+		return "FrameWrongSize\n"
+	cFrame = {}
+	for dataPoint in FRAME_STRUCT:	
+		if dataPoint.hasPositivity:
+			cFrame[dataPoint.name] = (int(decAssembly[0:1])*-2+1) * int(decAssembly[1:dataPoint.length]) * (float(10**dataPoint.decOffset))
+		else:
+			cFrame[dataPoint.name] = (int(decAssembly[0:dataPoint.length]) * (float(10**dataPoint.decOffset)))
+		
+		decAssembly = decAssembly[dataPoint.length:]
+	return cFrame
+
+'''def nicePrintout(data):
+	print(" Time: {0}".format(data["time"]), end = " ", flush = True)
+	print(" Flight mode: {0}".format(data["flight_mode"]), end = " ", flush = True)
+	print(" Squib Deployed: {0}".format(data["squib_deployed"]), end = " ", flush = True)
+	print(" Temperature: {0}".format(data["temp"]), end = " ", flush = True)
+	print(" Pressure: {0}".format(data["pressure"]), end = " ", flush = True)
+	print(" Current 1: {0}".format(data["current_1"]), end = " ", flush = True)
+	print(" Bus Voltage 1: {0}".format(data["volt_b1"]), end = " ", flush = True)
+	print(" Current 2: {0}".format(data["current_2"]), end = " ", flush = True)
+	print(" Bus Voltage 2: {0}".format(data["volt_b2"]), end = " ", flush = True)
+	print(" GPS Latitude: {0}".format(data["gps_lat"]), end = " ", flush = True)
+	print(" GPS Longitude: {0}".format(data["gps_lon"]), end = " ", flush = True)
+	print(" GPS Altitude: {0}".format(data["gps_alt"]), end = " ", flush = True)
+	print(" GPS Speed: {0}".format(data["gps_spd"]), end = " ", flush = True)
+	print(" Acceleration X: {0}".format(data["a_x"]), end = " ", flush = True)
+	print(" Acceleration Y: {0}".format(data["a_y"]), end = " ", flush = True)
+	print(" Acceleration Z: {0}".format(data["a_z"]), end = " ", flush = True)
+	print(" Magnetometer X: {0}".format(data["mag_x"]), end = " ", flush = True)
+	print(" Magnetometer Y: {0}".format(data["mag_y"]), end = " ", flush = True)
+	print(" Magnetometer Z: {0}".format(data["mag_z"]), end = "\r", flush = True)'''
+	     
+test={"time": 420, "flight_mode": 420, "squib_deployed": 420, "temp": 420, "pressure": 420, "current_1": 420, "volt_b1": 420, "current_2": 420, "volt_b2": 420, "gps_lat": 420, "gps_lon": 420, "gps_alt": 420, "gps_spd": 420, "a_x": 420, "a_y": 420, "a_z": 420, "mag_x": 420, "mag_y": 420, "mag_z": 420}
+
+FRAME_STRUCT = []
+FRAME_STRUCT.append(DataPoint("time", 0, 13, -3))
+FRAME_STRUCT.append(DataPoint("flight_mode", 0, 1, 0))
+FRAME_STRUCT.append(DataPoint("squib_deployed", 0, 1, 0))
+FRAME_STRUCT.append(DataPoint("temp", 0, 4, -1))
+FRAME_STRUCT.append(DataPoint("pressure", 0, 5, 1))
+FRAME_STRUCT.append(DataPoint("current_1", 0, 4, -1))
+FRAME_STRUCT.append(DataPoint("volt_b1", 0, 4, -3))
+FRAME_STRUCT.append(DataPoint("current_2", 0, 4, -1))
+FRAME_STRUCT.append(DataPoint("volt_b2", 0, 4, -3))
+FRAME_STRUCT.append(DataPoint("gps_lat", 1, 10, -6))
+FRAME_STRUCT.append(DataPoint("gps_lon", 1, 10, -6))
+FRAME_STRUCT.append(DataPoint("gps_alt", 0, 5, -1))
+FRAME_STRUCT.append(DataPoint("gps_spd", 0, 4, -1))
+FRAME_STRUCT.append(DataPoint("a_x", 1, 4, -1))
+FRAME_STRUCT.append(DataPoint("a_y", 1, 4, -1))
+FRAME_STRUCT.append(DataPoint("a_z", 1, 4, -1))
+FRAME_STRUCT.append(DataPoint("mag_x", 1, 4, 1))
+FRAME_STRUCT.append(DataPoint("mag_y", 1, 4, 1))
+FRAME_STRUCT.append(DataPoint("mag_z", 1, 4, 1))
+
+frames = []
+fCount = 0
+while True:
+	try:
+	    tempLine = ""
+	    if (ser.read(1) == "\x7F"):
+		if (ser.read(1) == "\x7F"):
+		    tempLine += ser.read(2)
+		    while ((tempLine[-1] != "\x00" or tempLine[-2] != "\x00") and len(tempLine) < (FRAMESIZE + 2)):
+			tempLine+=ser.read(1)
+		    if (tempLine[-1] == "\x00" and tempLine[-2] == "\x00" and len(tempLine) == (FRAMESIZE + 2)):
+			decodedData = binToDec(asciiToBin(tempLine[:-2]))
+			disassembledData = frameDisassembly(decodedData)
+			for key in disassembledData.keys():
+				disassembledData[key] = round(disassembledData[key], 3)
+			#rxfile.write(str(disassembledData))
+			#rxfile.write("\n")
+			frames.append(disassembledData)
+			output = """
+a_x: {a_x}g
+a_y: {a_y}g
+a_z: {a_z}g
+temp: {temp}°C
+pressure: {pressure}hPa
+current_1: {current_1}mA
+volt_b1: {volt_b1}V
+current_2: {current_2}mA
+volt_b2: {volt_b2}V
+gps_lon: {gps_lon}
+gps_lat: {gps_lat}
+gps_alt: {gps_alt}m
+gps_spd: {gps_spd}m/s
+mag_x: {mag_x} Gauss
+mag_y: {mag_y} Gauss
+mag_z: {mag_z} Gauss
+""".format(
+			a_x = disassembledData["a_x"],
+			a_y = disassembledData["a_y"],
+			a_z = disassembledData["a_z"],
+			temp = disassembledData["temp"],
+			pressure = disassembledData["pressure"],
+			current_1 = disassembledData["current_1"],
+			volt_b1 = disassembledData["volt_b1"],
+			current_2 = disassembledData["current_2"],
+			volt_b2 = disassembledData["volt_b2"],
+			gps_lon = disassembledData["gps_lon"],
+			gps_lat = disassembledData["gps_lat"],
+			gps_alt = disassembledData["gps_alt"],
+			gps_spd = disassembledData["gps_spd"],
+			mag_x = disassembledData["mag_x"],
+			mag_y = disassembledData["mag_y"],
+			mag_z = disassembledData["mag_z"]
+    	)
+			output = output.replace("\n","\n\033[K")
+                        write(output)
+                        lines = len(output.split("\n"))
+                        write("\033[{}A".format(lines - 1))
+                        time.sleep(1)
+                        fCount += 1
+                tempLine = ""
+	except (KeyboardInterrupt):
+		ser.close()
+		rxfile.close()
