6ed0908cf5438395e326d6f0d53364823f2a002a
ABLV2539 <alexander.vu@warriorlife.net>
2017-07-12 11:03:11 -0700

    Update ATCommandTest.py

--- testing/radio/ATCommandTest.py
+++ testing/radio/ATCommandTest.py
@@ -15,3 +15,22 @@ while char != "\r":
   char = ser.read()
 print(reply)
 print(reply.decode("utf-8"))
+ser.write("ATCN")
+
+def readATParam(ser, name):  
+  command = "AT" + name
+  ser.write(command)
+  reply = ""
+  char = ser.read()
+  while char != "\r":
+    reply += char
+    char = ser.read()
+  return reply.decode("utf-8")
+
+#ser.write("+++")
+#time.sleep(1)
+#readATParam(ser, ID)
+#readATParam(ser, HC)
+#readATParam(ser, MY)
+#readATParam(ser, DT)
+#readAtParam(ser, RS)
